c92e2c239869ed51805bb06786454ebbb4bafa62
Mukesh Suthar <mukesh@stepsetgo.com>
2021-12-05 11:26:11 +0530

    refact: rename discordScraper -> DiscordUtils.py

--- module/__init__.py
+++ module/__init__.py
@@ -1,4 +1,4 @@
 # Time to create a simple alias for the library when importing it.
-from .DiscordScraper import DiscordScraper
+from .DiscordUtils import DiscordScraper
 from .MongoUtils import MongoUtils
 from .BinanceUtils import BinanceUtils
